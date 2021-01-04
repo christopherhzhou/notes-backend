02e1a6178a802a4de143a4b1ae7365b73a9879b6
christopherzhou <christopherjzhou@gmail.com>
2021-01-03 20:59:25 -0500

    secret key

--- notes_backend/settings.py
+++ notes_backend/settings.py
@@ -1,11 +1,12 @@
 import os
 from datetime import timedelta
-from secret import secret_key
+import environ
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = secret_key
+environ.Env.read_env()
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = True
 
--- secret.py
+++
@@ -1,1 +0,0 @@
-secret_key = '-zh_3o%t3l+wjd%qj@6eh7_w-$le19c0l38fx$+phc8k1&xvg!'
